b249ab03be13036231dd98618eb4099f4c90fead
udbhav <udbhav@eCTF-VM.myguest.virtualbox.org>
2024-02-27 00:08:19 -0500

    building but signing hash with ecc key fails

--- application_processor/inc/crypto_test.h
+++ application_processor/inc/crypto_test.h
@@ -3,7 +3,7 @@
 #define ECTF_CRYPTO_TEST_H
 
 #include "wolfssl/wolfcrypt/ecc.h"
-//#include "wolfssl/wolfcrypt/hash.h"
+#include "wolfssl/wolfcrypt/hash.h"
 
 int create_keypair();
 
--- application_processor/src/crypto_test.c
+++ application_processor/src/crypto_test.c
@@ -36,25 +36,37 @@ int create_keypair() {
 	}
 
 	// HASH DATA
-	// char data[] = "Hello, this is a message for testing wolfSSL. The documentation is good but often confusing. I still don't know if wolfCrypt can work without dynamic memory or not.";
+	char data[] = "Hello, this is a message for testing wolfSSL. The documentation is good but often confusing. I still don't know if wolfCrypt can work without dynamic memory or not.";
 
-	// print_debug("Running SHA256 hash on data...");
-	// byte hash_out[WC_SHA256_DIGEST_SIZE];
-	// ret = wc_Sha256Hash((byte*) data, sizeof(data), hash_out);
+	print_debug("Running SHA256 hash on data...");
+	byte hash_out[WC_SHA256_DIGEST_SIZE];
+	ret = wc_Sha256Hash((byte*) data, sizeof(data), hash_out);
 
-	// if (ret != 0) {
-	// 	print_debug("Error in doing SHA256 hash");
-	// 	return -1;
-	// }
+	if (ret != 0) {
+		print_debug("Error in doing SHA256 hash");
+		return -1;
+	}
 
-	// print_debug("Hashed data: ");
-	// print_hex_debug(hash_out, WC_SHA256_DIGEST_SIZE);
+	print_debug("Hashed data: ");
+	print_hex_debug(hash_out, WC_SHA256_DIGEST_SIZE);
 
+	// SIGN HASH
+	byte sig_out[128];
+	int sigSz;
 
+	print_debug("Signing hash with ECC key... ");
+	ret = wc_ecc_sign_hash(hash_out, WC_SHA256_DIGEST_SIZE, sig_out, 128, &sigSz, &key);
+	if (ret != 0) {
+		print_debug("Error signing hash.");
+		return -1;
+	}
+	print_debug("Signed hash with signature length %d:", sigSz);
+	print_hex_debug(sig_out, sigSz);
+
+	// Free stuff
 	print_debug("'Freeing' WC_RNG and ECC key... (?)");
 	wc_ecc_key_free(&key);
 	ret = wc_FreeRng(&mRng);
 	print_debug("WC_RNG and ECC Key 'freed' with ret %d.", ret);
-
 
 }
