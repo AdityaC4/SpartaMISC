f7612eaba6a8e8454bcccd2b1d580a260ca15836
udbhav-s <udbhavs03@gmail.com>
2024-02-28 17:01:15 -0500

    add ap_hello message struct and primitives for verifying signed data

--- application_processor/src/crypto_test.c
+++ application_processor/src/crypto_test.c
@@ -9,10 +9,17 @@
 #include "wolfssl/wolfcrypt/integer.h"
 
 #define ECC_CURVE			ECC_SECP256R1
+#define ECC_KEY_LEN			32
+
+#define COMP_KEY_SIZE		33
+#define COMP_KEY_BUFSIZE	36 	// To avoid struct padding issues, just in case
+
 #define PUBKEY_BUF_LEN		ECC_BUFSIZE
 #define PUBKEY_LEN 			ECC_MAXSIZE+1
+
 #define POINT_SIZE			32
 #define CERT_DATA_SIZE		2 * POINT_SIZE + 4
+#define ECC_SIG_SIZE 		72
 // #define MAX_CERT_SIZE       4096
 
 typedef struct cert_data {
@@ -20,6 +27,31 @@ typedef struct cert_data {
 	byte pubkey_y[32];
 	word32 tag;
 } cert_data;
+
+typedef struct ap_hello {
+	// Compressed ANSI X9.63 keys
+	byte ap_pubkey[COMP_KEY_BUFSIZE];
+	byte ap_dh_pubkey[COMP_KEY_BUFSIZE];
+} ap_hello;
+
+typedef struct signed_ap_hello {
+	ap_hello hi;
+	byte hello_sig[ECC_SIG_SIZE];
+} signed_ap_hello;
+
+typedef struct signed_ap_hello_with_cert {
+	signed_ap_hello data;
+	byte cert_sig[ECC_SIG_SIZE];
+} signed_ap_hello_with_cert;
+
+int make_ecc_key(ecc_key* key, WC_RNG* rng) {
+	int ret = wc_ecc_init(key);
+	if (ret == 0) {
+		ret = wc_ecc_make_key_ex(rng, ECC_KEY_LEN, key, ECC_CURVE);
+	}
+
+	return ret;
+}
 
 int load_ap_private_key(ecc_key* key) {
 	int ret = wc_ecc_init(key);
@@ -53,11 +85,11 @@ int construct_ap_cert_data(cert_data* cert, ecc_key* ap_key) {
 	// mp_to_unsigned_bin stores point in buffer in big-endian format
 
 	byte point_x[POINT_SIZE];
-	ret = mp_to_unsigned_bin(&((ap_key->pubkey).x), point_x);
+	ret = mp_to_unsigned_bin((mp_int *) &((ap_key->pubkey).x), point_x);
 	if (ret != 0) return ret;
 
 	byte point_y[POINT_SIZE];
-	ret = mp_to_unsigned_bin(&((ap_key->pubkey).y), point_y);
+	ret = mp_to_unsigned_bin((mp_int *) &((ap_key->pubkey).y), point_y);
 	if (ret != 0) return ret;
 
 	// memcpy(cert->pubkey_x, (ap_key->pubkey).x[0].dp, POINT_SIZE);
@@ -67,14 +99,124 @@ int construct_ap_cert_data(cert_data* cert, ecc_key* ap_key) {
 	memcpy(cert->pubkey_y, point_y, POINT_SIZE);
 
 	cert->tag = (word32) 0xffffffff;
+
+	return ret;
 }
 
-int hash_cert(cert_data* cert, byte* hash_out) {
-	return wc_Sha256Hash((byte*) cert, CERT_DATA_SIZE, hash_out);
+int verify_data_signature(byte* data, word32 data_size, byte* sig, word32 sig_size, ecc_key* key) {
+	print_debug("Hashing data...");
+
+	byte hash_out[WC_SHA256_DIGEST_SIZE];
+	int ret = wc_Sha256Hash(data, data_size, hash_out);
+
+	if (ret != 0) {
+		print_debug("Error in doing SHA256 hash");
+		return -1;
+	}
+
+	print_debug("Hashed data: ");
+	print_hex_debug(hash_out, WC_SHA256_DIGEST_SIZE);
+
+	print_debug("Verifying signature...");
+
+	int stat = -1;
+	ret = wc_ecc_verify_hash(sig, sig_size, hash_out, WC_SHA256_DIGEST_SIZE, &stat, key);
+
+	if (stat != 1 || ret != 0) return -1;
+	else return 0;
 }
 
-int verifycert() {
-	print_info("In verifycert");
+int sign_data(byte* data, word32 data_size, byte* sig, word32* sig_size, ecc_key* key, WC_RNG* rng) {
+	byte hash_out[WC_SHA256_DIGEST_SIZE];
+	int ret = wc_Sha256Hash(data, data_size, hash_out);
+
+	if (ret != 0) {
+		print_debug("Error in doing SHA256 hash");
+		return -1;
+	}
+
+	ret = wc_ecc_sign_hash(hash_out, WC_SHA256_DIGEST_SIZE, sig, sig_size, rng, key);
+
+	return ret;
+}
+
+int create_hello() {
+	print_info("In create_hello()");
+
+	print_debug("Size of ap_hello struct: %d", sizeof(ap_hello));
+
+	int ret;
+
+	WC_RNG rng;
+	wc_InitRng(&rng);
+
+	signed_ap_hello_with_cert msg;
+	memset(&msg, 0, sizeof(msg));
+
+	ecc_key ap_key;
+	ret = load_ap_private_key(&ap_key);
+	if (ret != 0) {
+		print_debug("Error loading AP key: %d", ret);
+		return -1;
+	}
+
+	ecc_key ap_dh_key;
+	ret = make_ecc_key(&ap_dh_key, &rng);
+	if (ret != 0) {
+		print_debug("Error making AP DH key: %d", ret);
+		return -1;
+	}
+
+	word32 outLen = COMP_KEY_BUFSIZE;
+	ret = wc_ecc_export_x963_ex(&ap_key, msg.data.hi.ap_pubkey, &outLen, 1);
+	if (ret != 0) {
+		print_debug("Error exporting AP key to buffer: %d", ret);
+		return -1;
+	}
+	print_debug("Exported AP public key to buffer: wrote %d bytes", outLen);
+
+	outLen = COMP_KEY_BUFSIZE;
+	ret = wc_ecc_export_x963_ex(&ap_dh_key, msg.data.hi.ap_dh_pubkey, &outLen, 1);
+	if (ret != 0) {
+		print_debug("Error exporting AP DH key to buffer: %d", ret);
+		return -1;
+	}
+	print_debug("Exported AP DH public key to buffer: wrote %d bytes", outLen);
+
+	print_debug("Signing ap hello with ap public key");
+
+	byte sig_out[ECC_SIG_SIZE];
+	memset(sig_out, 0, ECC_SIG_SIZE);
+	word32 sig_sz = ECC_SIG_SIZE;
+
+	ret = sign_data((byte*) &msg.data.hi, (word32) sizeof(msg.data.hi), sig_out, &sig_sz, &ap_key, &rng);
+	if (ret != 0) {
+		print_debug("Error signing ap hello: %d", ret);
+		return -1;
+	}
+
+	print_debug("Setting AP signature in signed_ap_hello");
+
+	memcpy(&(msg.data.hello_sig), sig_out, sizeof(sig_out));
+
+	print_debug("Setting host certificate signature in msg");
+
+	byte host_cert[] = CERT_SIGNATURE;
+	memcpy(&(msg.cert_sig), host_cert, sizeof(host_cert));
+
+	print_debug("Completed construction of AP hello message: total size %d", sizeof(msg));
+	print_hex_debug((byte *) &msg, sizeof(msg));
+
+	print_debug("Freeing resources");
+	wc_ecc_key_free(&ap_key);
+	wc_ecc_key_free(&ap_dh_key);
+	ret = wc_FreeRng(&rng);
+
+	return ret;
+}
+
+int verify_self_cert() {
+	print_info("In verify_self_cert");
 
 	WC_RNG mRng;
 	wc_InitRng(&mRng);
@@ -96,8 +238,8 @@ int verifycert() {
 	}
 
 	print_debug("Dumping AP key x and y points: ");
-	print_hex_debug(ap_key.pubkey.x[0].dp, POINT_SIZE);
-	print_hex_debug(ap_key.pubkey.y[0].dp, POINT_SIZE);
+	print_hex_debug((byte*) ap_key.pubkey.x[0].dp, POINT_SIZE);
+	print_hex_debug((byte*) ap_key.pubkey.y[0].dp, POINT_SIZE);
 
 	print_debug("Creating certificate data from AP key");
 
@@ -110,34 +252,15 @@ int verifycert() {
 	print_debug("Certificate data:");
 	print_hex_debug((byte *) &cert, CERT_DATA_SIZE);
 
-	print_debug("Running SHA256 hash on certificate data");
-
-	byte hash_out[WC_SHA256_DIGEST_SIZE];
-	ret = hash_cert(&cert, hash_out);
-
-	if (ret != 0) {
-		print_debug("Error in doing SHA256 hash");
-		return -1;
-	}
-
-	print_debug("Hashed data: ");
-	print_hex_debug(hash_out, WC_SHA256_DIGEST_SIZE);
-
-	print_debug("Verifying signature...");
-
 	byte cert_sig[] = CERT_SIGNATURE;
 
-	int stat = -1;
-	ret = wc_ecc_verify_hash(cert_sig, (word32) sizeof(cert_sig), hash_out, WC_SHA256_DIGEST_SIZE, &stat, &host_pubkey);
+	ret = verify_data_signature((byte*) &cert, CERT_DATA_SIZE, cert_sig, (word32) sizeof(cert_sig), &host_pubkey);
 
 	if (ret != 0) {
-        print_debug("Signature verification returned error %d\n", ret);
+        print_debug("Signature verification failed");
         return -1;
     }
-    if (stat != 1) {
-        print_debug("Signature verification rejected %d\n", stat);
-        // return -1;
-    } else {
+    else {
     	print_debug("Signature verified successfully! ");
     }
 
@@ -147,7 +270,9 @@ int verifycert() {
 int create_keypair() {
 	print_info("In create_keypair()");
 
-	verifycert();
+	verify_self_cert();
+
+	create_hello();
 
 	WC_RNG mRng;
 	wc_InitRng(&mRng);
@@ -203,7 +328,7 @@ int create_keypair() {
 	// SIGN HASH
 	byte sig_out[ECC_MAX_SIG_SIZE]; // An ECC signature is twice the length of the private key
 	memset(sig_out, 0, ECC_MAX_SIG_SIZE);
-	word32 sigSz;
+	word32 sigSz = ECC_SIG_SIZE;
 
 	print_debug("Signing hash with ECC key... ");
 	ret = wc_ecc_sign_hash(hash_out, WC_SHA256_DIGEST_SIZE, sig_out, &sigSz, &mRng, &key);
