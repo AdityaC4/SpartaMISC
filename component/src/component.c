32bf1d960abf2c6ff51642a201c247dfd3a95a0a
Charles <charles.selipsky@gmail.com>
2024-03-02 23:05:23 -0500

    Update component.c

--- component/src/component.c
+++ component/src/component.c
@@ -197,7 +197,8 @@ void process_validate() {
 
 void process_attest() {
     // The AP requested attestation. Respond with the attestation data
-    uint8_t len = snprintf((char*)transmit_buffer, strlen((const char*)transmit_buffer), "LOC>%s\nDATE>%s\nCUST>%s\n",
+    #needs to be fixed to be memory safe
+    uint8_t len = sprintf((char*)transmit_buffer, "LOC>%s\nDATE>%s\nCUST>%s\n",
                 ATTESTATION_LOC, ATTESTATION_DATE, ATTESTATION_CUSTOMER) + 1;
     send_packet_and_ack(len, transmit_buffer);
 }
