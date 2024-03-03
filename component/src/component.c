87cacd8fc8cb384928ccc4bf0973637df0dd708b
udbhav-s <50021387+udbhav-s@users.noreply.github.com>
2024-03-03 18:36:20 -0500

    Update component.c

--- component/src/component.c
+++ component/src/component.c
@@ -96,7 +96,11 @@ uint8_t transmit_buffer[MAX_I2C_MESSAGE_LEN];
  * This function must be implemented by your team to align with the security requirements.
 */
 void secure_send(uint8_t* buffer, uint8_t len) {
-    send_packet_and_ack(len, buffer); 
+    byte send_buf[MAX_I2C_MESSAGE_LEN];
+    bzero(send_buf, sizeof(send_buf));
+    memcpy(send_buf, buffer, len);
+    
+    send_packet_and_ack(MAX_I2C_MESSAGE_LEN, send_buf); 
 }
 
 /**
